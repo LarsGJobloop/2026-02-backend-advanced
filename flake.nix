936bd937a13b94d8ad98ee190021fdd18a6d32e8
zabronax <zabronax@gmail.com>
2026-01-19 10:44:09 +0100

    chore: define development environment

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "Backend Advanced";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      withSystem = nixpkgs.lib.genAttrs [
+        "x86_64-linux"
+        "x86_64-darwin" # TODO! NOT Verified
+        "aarch64-linux" # TODO! NOT Verified
+        "aarch64-darwin"
+      ];
+      withPkgs = callback: withSystem (system: callback (import nixpkgs { inherit system; }));
+    in
+    {
+      devShells = withPkgs (pkgs: {
+        default = pkgs.mkShell {
+          packages = with pkgs; [
+            dotnetCorePackages.sdk_10_0
+          ];
+        };
+      });
+
+      formatter = withPkgs (pkgs: pkgs.nixfmt-rfc-style);
+    };
+}
