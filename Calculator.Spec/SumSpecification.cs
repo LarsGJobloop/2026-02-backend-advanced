41dc12fd48bf5d02ea295e2abe48a9d7116cf8b1
zabronax <zabronax@gmail.com>
2026-01-19 11:06:49 +0100

    feat(spec): add sum specification

---
+++ Calculator.Spec/SumSpecification.cs
@@ -0,0 +1,20 @@
+﻿namespace Calculator.Spec;
+
+public class SumSpecification
+{
+    [Fact]
+    public void SumOfTwoNumbers_ShouldReturnCorrect_Sum()
+    {
+        // Arrange
+        var a = 2;
+        var b = 2;
+        var expectedResult = 4;
+        var calculator = new Calculator();
+
+        // Act
+        var result = calculator.Sum(a, b);
+
+        // Assert
+        Assert.Equal(expectedResult, result);
+    }
+}
