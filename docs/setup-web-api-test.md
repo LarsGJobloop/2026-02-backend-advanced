06de52fca11cee81c0dfa83e1a4ca0d926bc8d5d
zabronax <zabronax@gmail.com>
2026-01-20 10:24:55 +0100

    feat(spec): add health probe spec

--- docs/setup-web-api-test.md
+++ docs/setup-web-api-test.md
@@ -1,7 +1,13 @@
 # Setup Web API Tests
 
 ```sh
+# Scaffold new project
 dotnet new xunit --name <test-suit-name> --output <project-folder>
+# Add Web API testing package
 dotnet add <project-folder> package  Microsoft.AspNetCore.
 Mvc.Testing
+# Add System Under Test (SUT) to test project
+dotnet add <test-project> reference <project-folder>
+# Add test project to solution
+dotnet sln add spec/MemoryService.Spec
 ```
---
+++ spec/MemoryService.Spec/Probes.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace MemoryService.Spec;
+
+public class HealthProbe
+    : IClassFixture<WebApplicationFactory<Program>>
+{
+    private readonly WebApplicationFactory<Program> _factory;
+
+    public HealthProbe(WebApplicationFactory<Program> factory)
+    {
+        _factory = factory;
+    }
+
+    [Fact]
+    public async Task What_To_Test_First()
+    {
+        // As a client
+        var client = _factory.CreateClient();
+
+        // When I try to probe the health endpoint
+        var response = await client.GetAsync("/healthz");
+
+        // The server responds with a success status code
+        response.EnsureSuccessStatusCode();
+    }
+}
