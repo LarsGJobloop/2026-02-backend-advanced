c2b9980bd410bda3755e77b04dc5d41ab34aecac
zabronax <zabronax@gmail.com>
2026-02-10 11:08:41 +0100

    add authorization requirement for weather controller

--- src/WeatherService/Controllers/WeatherForecastController.cs
+++ src/WeatherService/Controllers/WeatherForecastController.cs
@@ -1,7 +1,9 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace WeatherSerice.Controllers;
 
+[Authorize]
 [ApiController]
 [Route("[controller]")]
 public class WeatherForecastController : ControllerBase
